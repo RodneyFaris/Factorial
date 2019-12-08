6ac66d32d5b41a17fbc546f75217dae3fa39f4c5
Rodney Faris <rodney.faris@gmail.com>
2019-12-07 19:25:52 -0800

    Add project files.

---
+++ Factorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factorial
+{
+    public class Factorial
+    {
+        public int Calculate(int value)
+        {
+            double F(double fact)
+            {
+                value -= 1;
+                if (value <= 1) return fact;
+                
+                return fact * F(value);
+            }
+            var result = F(value);
+            return result >= int.MaxValue ? -1 : (int)result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += new ConsoleCancelEventHandler((s, a) =>
+            {
+                Console.Clear();
+                a.Cancel = false;
+                Environment.Exit(0);
+            });
+
+            var factorial = new Factorial();
+
+            do
+            {
+                Console.Write("Enter number: ");
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out int value))
+                {
+                    var fact = factorial.Calculate(value);
+                    var result = fact == -1 ? "Overflow error" : $"Factorial of {value}: {fact}";
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number");
+                }
+            } while (true);
+        }
+    }
+}
